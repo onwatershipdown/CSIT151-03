e38b06ed51802d65ac1e5bce82e57b075786dfaa
Matt Passarelli <pass3805@fredonia.edu>
2022-02-12 21:47:52 -0500

    Initial Commit

---
+++ CSIT151-03/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSIT151-03
+//
+//  Created by Matthew Passarelli on 2/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
